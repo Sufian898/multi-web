e2a6fe13aca58de1bbc22a0944803e06a8647ecf
Sufian <167395097+Sufian898@users.noreply.github.com>
2025-12-19 20:59:49 +0500

    Delete backend directory

--- backend/routes/contactMessageRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createContactMessage, getContactMessageByTrackingCode } from '../controllers/contactMessageController.js';
-
-const router = express.Router();
-
-router.post('/', createContactMessage);
-router.get('/track/:code', getContactMessageByTrackingCode);
-
-export default router;
