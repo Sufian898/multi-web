5d7aff7852b84b220825f000624a4d1fc3126be9
Sufian <167395097+Sufian898@users.noreply.github.com>
2025-12-26 17:47:06 +0500

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,13 +1,20 @@
 import connectDB from '../config/database.js';
 import app from '../serverApp.js';
 
+let isConnected = false;
+
 export default async function handler(req, res) {
   try {
-    console.log('[api]', req.method, req.url);
-    await connectDB();        // DB connect
-    return app(req, res);     // Express ko handover
-  } catch (e) {
-    console.error('Serverless handler error:', e);
-    return res.status(500).json({ message: 'Server error' });
+    console.log('[API]', req.method, req.url);
+
+    if (!isConnected) {
+      await connectDB();
+      isConnected = true;
+    }
+
+    return app(req, res);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
   }
 }
